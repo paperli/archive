3065c1ac2476a18c49855638af874a3ae4517d13
Paper <paper.li@gmail.com>
2015-07-27 18:38:07 +0800

    Add mobile version website

---
+++ m/js/main.js
@@ -0,0 +1,368 @@
+var cases_data = [
+{
+		title:'Land Rover China',
+		article:'<p>Land Rover China was building their manufacturing chain in China. In MobiApps, we cooperated with Shanghai team and developed the sales channel system for customers and dealers. With our interaction model, service agents and car owners could communicate through instant messaging and managing service booking in hand.</p><p>The clever of the model solved the difficulty of integration of multiple dealer\'s management system. It also improved Land Rover China\'s control on first-hand saleforce and customer feedback.</p>',
+		bg_url:['images/landrover1.jpg','images/landrover2.jpg','images/landrover3.jpg','images/landrover4.jpg','images/landrover5.jpg','images/landrover6.jpg'],
+		img_id:'bg_landrover',
+		canvas_id:'canvas_landrover'
+	},
+{
+		title:'Qute',
+		article:'<p><a href=\'https://itunes.apple.com/vc/app/qute-qr-code-2d-barcode-scanner/id828181167?mt=8\' target=\'_blank\'>@App Store</a></p><p>Qute is not just the QR code scanner. It also brings the QR code online. People can share and read the comments about the QR code from the world.</p><p>The idea was from the scenarios from street. Image that you saw a QR code on the street. You realized it\'s shit only when you already visit through it. With Qute people can see the comments from the world about the QR code and you decide to continue or abort the visit.</p><p>Beyond that, Qute keeps the scan picture and displays it as part of result. Not only for the visual, but also for the memory. People may recall where the QR code is by the surrending of the QR code in the picture.</p>',
+		bg_url:['../images/s/qute1.jpg','../images/s/qute2.jpg','../images/s/qute3.jpg','../images/s/qute4.jpg','../images/s/qute5.jpg'],
+		img_id:'bg_qute',
+		canvas_id:'canvas_qute',
+		base_color:'#b8b0cd',
+		text_color:'#05115a'
+	},
+{
+		title:'GamerChat',
+		article:'<p><a href=\'https://itunes.apple.com/us/app/gong-hui-tong/id731810260?ls=1&mt=8\' target=\'_blank\'>@App Store</a></p><p>People shall be able to talk at any time, any where, even they are offline of the online game.</p><p>GamerCaht is an app to let Gamers to continue talking on the go. The app inherit the nature of online game: Multi-role for single person, Guild(Group) communication, and Fun-factor passcode. The piano was designed to play the passcode to integrate the joy in practice. The multi-role system was designed to bring gamer\'s experience into chatting.</p>',
+		//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
+		bg_url:['../images/s/gamerchat1.jpg','../images/s/gamerchat2.jpg','../images/s/gamerchat3.jpg','../images/s/gamerchat4.jpg','../images/s/gamerchat5.jpg'],
+		img_id:'bg_gamerchat',
+		canvas_id:'canvas_gamerchat',
+		base_color:'#b8b0cd',
+		text_color:'#05115a'
+	},
+	{
+			title:'Touchbook',
+			article:'<p>Create your travel memory easily on-the-go!</p><p>The app provided the leasure way to create the memory book with traveling pictures. The inventing instruction and realistic theme are applied in the app.</p>',
+			//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
+			bg_url:['../images/s/touchbook2.jpg','../images/s/touchbook3.jpg','../images/s/touchbook4.jpg'],
+			img_id:'bg_touchbook',
+			canvas_id:'canvas_touchbook',
+			base_color:'#d9dade',
+			text_color:'#13636b'
+		},
+		{
+				title:'MobiApps',
+				article:'<p><a href=\'https://itunes.apple.com/tw/app/ding-hao/id577065907?mt=8\' target=\'_blank\'>Mobiapps @App Store</a></p><p>In end of 2012, I joined the joyful team MobiApps to lead the UX and UI design. We server the mobile app for retail sale and small business. With rapid design pattern, the mobile app can be prompt built and delivered. The flexibility of the team allowed me to break out serveral concepts including smart no-content mode, social biz UI, etc.. Still, I used variable tools like Dropbox or TAP to rapidly test new concept.</p>',
+				//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
+				bg_url:['../images/s/mobiapps10.jpg','../images/s/mobiapps8.jpg','../images/s/mobiapps9.jpg','../images/s/mobiapps11.jpg','../images/s/mobiapps12.jpg','../images/s/mobiapps4.jpg','../images/s/mobiapps5.jpg'],
+				img_id:'bg_mobiapps_app',
+				canvas_id:'canvas_mobiapps_app',
+				base_color:'#ddd3d3',
+				text_color:'#6a1050'
+			},
+{
+		title:'Pilotfish MCBW event',
+		article:'<p><a href=\'http://www.pilotfish.eu/mcbw/\' target=\'_blank\'>http://www.pilotfish.eu/mcbw/</a></p><p>The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. <b>It\'s all about time!</b> The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.</p>',
+		//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
+		bg_url:['../images/s/mcbw-1.jpg','../images/s/mcbw-2.jpg'],
+		img_id:'bg_mcbw',
+		canvas_id:'canvas_mcbw',
+		base_color:'#dbd8d3',
+		text_color:'#683011'
+	},
+	{
+			title:'Pilotfish Mobile Website',
+			article:'<p><a href=\'http://www.pilotfish.eu/\' target=\'_blank\'>http://www.pilotfish.eu/</a></p><p>Pilotfish goes to mobile!</p><p>Pilotfish was reforming official website to meet fashion web spec, aka. no flash, go mobile. I was in the team to redesign the flow/wireframe of new offical website (include desktop and mobile version). I made bunch of demo units in HTML to present flow, transition and movement to developer outsourcing.</p><p>The website was announced in end 2012.</p>',
+			//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
+			bg_url:['../images/s/pf_mobile1.jpg','../images/s/pf_mobile5.jpg','../images/s/pf_mobile6.jpg','../images/s/pf_mobile8.jpg','../images/s/pf_mobile4.jpg'],
+			img_id:'bg_pf_mobile',
+			canvas_id:'canvas_pf_mobile',
+			base_color:'#e0cbb7',
+			text_color:'#6d1702'
+		},
+	
+	{
+			title:'MobiApps Website',
+			article:'<p><a href=\'http://www.themobiapps.com\' target=\'_blank\'>http://www.themobiapps.com/</a></p><p>TheMobiApps, the mobile app start-up in Taiwan, was planning to build the company identify prior to media publishing. I designed the whole new website with the 4 key colors in the company logo. The website consisted of the touch keys in present design trend, one-page scroll, simple and html5. It is planned to publish in March, 2013.</p>',
+			//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
+			bg_url:['../images/s/mobiapps_web1.jpg','../images/s/mobiapps_web2.jpg','../images/s/mobiapps_web3.jpg'],
+			img_id:'bg_mobiapps',
+			canvas_id:'canvas_mobiapps',
+			base_color:'#c8d5cc',
+			text_color:'#29620e'
+		},
+		{
+				title:'DDD',
+				article:'<p>We count for Diabetics!</p><p>Diabetics count after dining for blood sugar balance. It may be simple counting for adults, but for kid it\'s the problem.</p><p>I was joining the team in pilotfish for solving this matter. The project contained two parts: iPhone Case with weight scale and the app to count the sugar contained. I made the wired scale for food weight and passed in cloud. The app count the sugar quantity based on online database and the food weight in cloud. The demo kit proved the concept and gain the success in the in-house design competition.</p>',
+				bg_url:['../images/s/ddd3.jpg','../images/s/ddd2.jpg','../images/s/ddd1.jpg','../images/s/ddd_3.jpg'],
+				img_id:'bg_ddd',
+				canvas_id:'canvas_ddd',
+				base_color:'#87b2c7',
+				text_color:'#032b42'
+			}];
+
+var prefix = [];
+
+var IMG_SCALE = 1.4; //140% of dev width
+
+var IMG_CENTER = -(IMG_SCALE-1)*100/2; // -20%
+var IMG_LEFT_EDGE = 0; // 0%
+var IMG_RIGHT_EDGE = -(IMG_SCALE-1)*100; // -40%
+var TILT_LEFT_EDGE = 8;
+var TILT_RIGHT_EDGE = -8;
+
+//navbar height control
+var NAVBAR_MIN_HEIGHT = 44;
+var NAVBAR_MAX_HEIGHT = 213;
+
+//TODO: use device width
+var IMG_WIDTH = 320;
+
+//TODO: mutiple cases
+var current_img = [];
+
+//TODO: mutiple cases
+var max_imgs = [];
+
+var speed = 500;
+
+//TODO: mutiple cases
+var pics = [];
+
+var isAndroid = false;
+
+var swipeOptions=
+{
+	triggerOnTouchEnd : true,	
+	swipeStatus : swipeStatus,
+	allowPageScroll:"vertical",
+	threshold:75			
+}
+
+prefix.push($('#doSupport').html());
+prefix.push($('#doTiltLR').html());
+prefix.push($('#doTiltFB').html());
+prefix.push($('#doDirection').html());
+
+if (window.DeviceOrientationEvent){
+	$('#doSupport').html(prefix[0]+'YES');
+	window.addEventListener('deviceorientation',devOrientHandler,false);
+	
+} else {
+	$('#doSupport').html(prefix[0]+'NO');
+}
+
+function devOrientHandler(evt){
+	var tiltLR = evt.gamma;
+	
+	var tiltFB = evt.beta;
+	
+	var dir = evt.alpha;
+	
+	$('#doTiltLR').html(prefix[1]+tiltLR);
+	$('#doTiltFB').html(prefix[2]+tiltFB);
+	$('#doTiltDirection').html(prefix[3]+dir);
+	
+	var angle;
+	//alert("go");
+	//cal tilt angle
+		//angle = IMG_RIGHT_EDGE + (IMG_LEFT_EDGE-IMG_RIGHT_EDGE)*(tiltLR-(TILT_RIGHT_EDGE))/(IMG_LEFT_EDGE-IMG_RIGHT_EDGE);
+		angle = IMG_LEFT_EDGE + (IMG_RIGHT_EDGE-IMG_LEFT_EDGE)*(tiltLR-TILT_LEFT_EDGE)/(TILT_RIGHT_EDGE-TILT_LEFT_EDGE);
+		if (angle > IMG_LEFT_EDGE){
+			angle = 0;
+		} else if (angle < IMG_RIGHT_EDGE) {
+			angle = IMG_RIGHT_EDGE;
+		} else {
+			angle = angle;
+		}
+		//angle = 10;
+		//alert(angle);
+		$('#doSupport').html(prefix[0]+'YES '+angle);
+		$('img#post').css('left',angle+'%');
+	
+}
+
+$(document).ready(function(){
+	//init
+	
+	// check if running on android
+	var ua = navigator.userAgent.toLowerCase();
+	isAndroid = ua.indexOf("android") > -1;
+	
+	
+	IMG_WIDTH = $(window).width();
+	
+	//$('#navbar').hide();
+	$(document).on('touchmove',function(e){
+		//console.log($(document).scrollTop());
+		if ($(document).scrollTop() >= NAVBAR_MAX_HEIGHT-NAVBAR_MIN_HEIGHT){
+			$('#navbar').height(NAVBAR_MIN_HEIGHT);
+		} else if ($(document).scrollTop() <= 0){
+			$('#navbar').height(NAVBAR_MAX_HEIGHT)
+		} else {
+			$('#navbar').height(NAVBAR_MAX_HEIGHT-$(document).scrollTop());
+		}
+	});
+	
+	$(document).on('touchend',function(e){
+		// To prevent in-smooth scroll evt 
+		if ($(document).scrollTop() >= NAVBAR_MAX_HEIGHT-NAVBAR_MIN_HEIGHT){
+			$('#navbar').height(NAVBAR_MIN_HEIGHT);
+		} else if ($(document).scrollTop() <= 0){
+			$('#navbar').height(NAVBAR_MAX_HEIGHT)
+		} else {
+			$('#navbar').height(NAVBAR_MAX_HEIGHT-$(document).scrollTop());
+		}
+	});
+	
+	if (isAndroid){
+		//alert('its on android');
+		$(document).scroll(function(e){
+			//console.log($(document).scrollTop());
+			if ($(document).scrollTop() >= NAVBAR_MAX_HEIGHT-NAVBAR_MIN_HEIGHT){
+				$('#navbar').height(NAVBAR_MIN_HEIGHT);
+			} else if ($(document).scrollTop() <= 0){
+				$('#navbar').height(NAVBAR_MAX_HEIGHT)
+			} else {
+				$('#navbar').height(NAVBAR_MAX_HEIGHT-$(document).scrollTop());
+			}
+		});
+	}
+	
+	$('#floor1_box>.case_description_box').click(function(e){
+		$(this).children().animate({
+			top:'+=60px'
+		},'fast');
+		$(this).fadeOut('fast');
+		$('#floor1_detail_box').delay(100).slideDown('slow');
+	});
+	
+	$('#floor1_detail_box>a.close_button').click(function(e){
+		$(this).parent().slideUp('fast');
+		$('#floor1_box>.case_description_box').delay(100).fadeIn('slow');
+		$('#floor1_box>.case_description_box').children().animate({
+			top:'-=60px'
+		},'slow');
+		return false;
+	});
+	
+	$('#floor1_detail_box>div.shadow').click(function(e){
+		$(this).parent().slideUp('fast');
+		$('#floor1_box>.case_description_box').delay(100).fadeIn('slow');
+		$('#floor1_box>.case_description_box').children().animate({
+			top:'-=60px'
+		},'slow');
+	});
+	
+	//pics[0] = $('#floor1_box>.pics_box');
+	//pics[0].swipe( swipeOptions );
+	
+	// Assign case data
+	for (var key in cases_data){
+		$("body").loadTemplate($("#template"),cases_data[key],{append:true});
+		
+		//set current viewing image at
+		current_img.push(0);
+		
+	}
+	
+	// Apply action in each case
+	$("div.case_whole_box").each(function(key,element){
+		//alert($(element).find("div.pics_box").length);
+		
+		// assign pcis
+		var pics_temp = cases_data[key]["bg_url"];
+		var id_base = cases_data[key]["img_id"];
+		var pic_temp;
+		
+		max_imgs.push(pics_temp.length)
+		
+		$(element).find("div.pics_box").width(IMG_WIDTH*pics_temp.length);
+		for (var i in pics_temp){
+			pic_temp = $("<img />").addClass("case_pic").attr("id",id_base+i).attr("src",pics_temp[i]).css('width',IMG_WIDTH);
+			$(element).find("div.pics_box").append(pic_temp);
+		}
+		
+		$(element).find("div.pics_box").attr("id","case_pics"+key);
+		pics.push($(element).find("div.pics_box"));
+		pics[key].swipe( swipeOptions );
+		
+		// Assign detail box colors
+		$(element).children('div.detail_box').css('backgroundColor',cases_data[key]['base_color']).css('color',cases_data[key]['text_color']);
+		$(element).children('div.detail_box').find('a').css('color',cases_data[key]['text_color']);
+		
+		// show/hide detail box
+		$(element).find('div.case_description_box').click(function(e){
+			$(this).children().animate({
+				top:'+=60px'
+			},'fast');
+			$(this).fadeOut('fast');
+			$(element).find('div.detail_box').delay(100).slideDown('slow');
+		});
+	
+		$(element).find('a.close_button').click(function(e){
+			$(this).parent().slideUp('fast');
+			$(element).find('div.case_description_box').delay(100).fadeIn('slow');
+			$(element).find('div.case_description_box').children().animate({
+				top:'-=60px'
+			},'slow');
+		});
+	
+		$(element).find('div.shadow').click(function(e){
+			$(this).parent().slideUp('fast');
+			$(element).find('div.case_description_box').delay(100).fadeIn('slow');
+			$(element).find('div.case_description_box').children().animate({
+				top:'-=60px'
+			},'slow');
+		});
+	});
+	
+});
+
+/**
+* Catch each phase of the swipe.
+* move : we drag the div.
+* cancel : we animate back to where we were
+* end : we animate to the next image
+*/			
+function swipeStatus(event, phase, direction, distance)
+{
+	//Get current interaction pics box id
+	var current_key = parseInt($(this).attr('id').split("case_pics")[1],10);
+	
+	//If we are moving before swipe, and we are going Lor R in X mode, or U or D in Y mode then drag.
+	if( phase=="move" && (direction=="left" || direction=="right") )
+	{
+		var duration=0;
+		
+		if (direction == "left")
+			scrollImages((IMG_WIDTH * current_img[current_key]) + distance, duration, current_key);
+		
+		else if (direction == "right")
+			scrollImages((IMG_WIDTH * current_img[current_key]) - distance, duration, current_key);
+		
+	}
+	
+	else if ( phase == "cancel")
+	{
+		scrollImages(IMG_WIDTH * current_img[current_key], speed, current_key);
+	}
+	
+	else if ( phase =="end" )
+	{
+		if (direction == "right")
+			previousImage(current_key);
+		else if (direction == "left")			
+			nextImage(current_key);
+	}
+}
+
+function previousImage(current_key)
+{
+	current_img[current_key] = Math.max(current_img[current_key]-1, 0);
+	scrollImages( IMG_WIDTH * current_img[current_key], speed, current_key);
+}
+
+function nextImage(current_key)
+{
+	current_img[current_key] = Math.min(current_img[current_key]+1, max_imgs[current_key] - 1);
+	scrollImages( IMG_WIDTH * current_img[current_key], speed, current_key);
+}
+
+/**
+* Manuallt update the position of the imgs on drag
+*/
+function scrollImages(distance, duration, current_key)
+{
+	pics[current_key].css("-webkit-transition-duration", (duration/1000).toFixed(1) + "s");
+	
+	//inverse the number we set in the css
+	var value = (distance<0 ? "" : "-") + Math.abs(distance).toString();
+	
+	pics[current_key].css("-webkit-transform", "translate3d("+value +"px,0px,0px)");
+}
